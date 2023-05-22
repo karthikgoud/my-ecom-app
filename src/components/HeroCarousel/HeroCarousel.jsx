7d4b64be1125e4dd5a6809a86aaf5ab42cf8a6c4
karthikgoud <karthikrv09@gmail.com>
2023-05-22 16:55:46 +0530

    feat - added show now btn

--- src/components/HeroCarousel/HeroCarousel.jsx
+++ src/components/HeroCarousel/HeroCarousel.jsx
@@ -1,9 +1,16 @@
+import { NavLink } from "react-router-dom";
 import "./HeroCarousel.css";
+import { useData } from "../../context/DataContext";
 
 const HeroCarousel = () => {
+  const { dispatch } = useData();
+
   return (
     <div className="hero-container">
       <img src="images/hero-img-1.jpg" alt="hero-image" />
+      <NavLink className="btn-shop-now" to="/product">
+        <button onClick={() => dispatch({ type: "RESET" })}>Shop Now</button>
+      </NavLink>
     </div>
   );
 };
